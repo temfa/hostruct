b80fd7d28aa2f6efaeb34ceb2e13d767b29ab4d1
temfa <akinfetemitope5@gmail.com>
2025-05-26 02:16:29 +0100

    Worked on the cards page

---
+++ components/booking-form/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+export const BookingForm = () => {
+  return (
+    <div className={styles.container}>
+      <h2>Hello WOrld</h2>
+    </div>
+  );
+};
